722b211119ce84f97402f9b8060090ff694756a9
paulopacitti <ppacitti@outlook.com>
2021-04-23 10:39:48 -0300

    feat: adding first data-structures, like Customer

--- sync_sushi.c
+++ sync_sushi.c
@@ -1,7 +1,27 @@
+#include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "customer.h"
+
+#define BAR_SIZE 5 // number of seats in the bar
+#define CUSTOMERS_SIZE 20 // number of customers;
+
+sem_t mutex;
+sem_t block;
+int customers_eating = 0;
+int customers_waiting = 0;
+int must_wait = 0;
+
+Customer customers[CUSTOMERS_SIZE]; // collection of customers;
 
 int main()
 {
-  printf("Hey\n");
-  return 1;
+	for(int i=0; i < CUSTOMERS_SIZE; i++) 
+  {
+	  customers[i].id = i;
+	  customers[i].status = waiting;
+    printf("Customer { id: %d, status: %d } \n", customers[i].id, customers[i].status);
+	}
+  return 0;
 }
